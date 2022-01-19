5bc28ea5162e319aeceac09bfc3da4a552b89aa8
unknown <limarcelo90@gmail.com>
2022-01-18 22:00:45 -0300

    first commit

---
+++ src/main/java/br/com/planningpoker/PlanningPokerOnlineApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.planningpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerOnlineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningPokerOnlineApiApplication.class, args);
+	}
+
+}
